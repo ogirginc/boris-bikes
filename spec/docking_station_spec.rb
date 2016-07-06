647d6c6a4419a1fb68a78fe86dbffc850805a2de
Robert Summers <rsummers02@gmail.com>
2016-07-06 15:22:22 +0100

    ch10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,15 @@
 require 'docking_station'
+
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
+
   it 'release_bike should create Bike class' do
     expect(subject.release_bike).to be_instance_of(Bike)
   end
+
+  it 'when the instance of Bike calls workign? should be true' do
+    expect(subject.release_bike).to be_working
+  end
+
 end
