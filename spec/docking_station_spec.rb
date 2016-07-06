306b67b1428e162becce27c4391f16d921bc5b76
ogirginc <ogirginc@gmail.com>
2016-07-06 15:06:21 +0100

    Add DockingStation::release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,7 @@
 require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-
-=begin
-  it 'response to the method release_bike' do
-#   docking_station = DockingStation.new
-   expect(DockingStation.new.respond_to?(:release_bike)).to eq true
- end
-=end
+  it 'release_bike should create Bike class' do
+    expect(subject.release_bike).to be_instance_of(Bike)
+  end
 end
