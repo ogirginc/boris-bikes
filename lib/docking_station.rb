041fd5676d92c2831f74fb8ddab7690c31b441ed
ogirginc <ogirginc@gmail.com>
2016-07-06 15:53:32 +0100

    Add some test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,4 +4,7 @@ class DockingStation
   def release_bike
     Bike.new
   end
+  def return_bike
+    true
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,6 @@
 require 'docking_station'
 
 describe DockingStation do
-
   it { is_expected.to respond_to :release_bike }
 
   it 'release_bike should create Bike class' do
@@ -12,4 +11,14 @@ describe DockingStation do
     expect(subject.release_bike).to be_working
   end
 
+  it "dock the user's bike at the docking station" do
+    expect(subject).to respond_to :return_bike
+  end
+
+  it 'user docked the bike at the docking station' do
+    expect(subject.return_bike).to be true
+  end
+  it 'check if there are bikes at the docking station' do
+    expect(subject.release_bike(@)
+  end
 end
