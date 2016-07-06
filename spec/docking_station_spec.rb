49d23518a5d4830f19f6ea295eea8536df606514
ogirginc <ogirginc@gmail.com>
2016-07-06 13:00:51 +0100

    ch8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
-require './docking_station.rb'
+require './lib/docking_station.rb'
 describe DockingStation do
-
-  
+ it 'response to the method release_bike' do
+#   docking_station = DockingStation.new
+   expect(DockingStation.new.respond_to?(:release_bike)).to eq true
+ end
 end
